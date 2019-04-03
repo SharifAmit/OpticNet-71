3d071b85390463f6b37d8e77a6b91e07550fac06
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2019-04-04 00:36:44 +0600

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-python3 train.py --datadir=/home/sharif/Desktop/OCT_classification/OCT_2nd_dataset --dataset=Srinivasan2014 --epoch=30 --logdir=./optic-net-log-rand/ --snapshot_name=Random_net --batch=8
-
-python3 train.py --datadir=/home/sharif/Desktop/OCT_classification/OCT2017/OCT2017 --dataset=Kermany2018 --epoch=30 --logdir=./optic-net-log-rand/ --snapshot_name=Random_net --batch=8
